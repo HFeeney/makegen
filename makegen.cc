d71cf272d45df807ff8dc03838ea73d96c2c6018
Ryan Nelson <ryannelson6@hotmail.com>
2024-03-27 23:25:45 -0700

    Reimplemented main() with better argument parsing
    Added ability to set default field values, intended for user to set
    aliases if they want to change defaults

--- makegen.cc
+++ makegen.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdexcept>
+#include <cstring>
 
 #include "Reader.h"
 #include "Crawler.h"
@@ -10,6 +11,13 @@
 using std::cout;
 using std::endl;
 using std::runtime_error;
+
+/**
+ * Returns true if the two cstrings are equal (identical)
+*/
+inline bool streq(const char* lhs, const char* rhs) {
+    return strcmp(lhs, rhs) == 0;
+}
 
 // Initialize the makefile struct
     // Check if a makefile exists already
@@ -24,26 +32,64 @@ using std::runtime_error;
                 // line.
 
 int main(int argc, char** argv) {
-    try {
-        Makefile m;
-        if (argc != 1) {
-            if (!(argc == 2 && argv[2][0] == '-' && argv[2][1] == 'n' && argv[2][2] == '\0')) {
-                // invalid
-                cout << "Usage: " << argv[0] << endl;
-                return EXIT_FAILURE;
-            }
-            // force new
-        } else {
-            // try read
-            m = read_makefile();
+    // parse options
+    bool forceNew = false;
+    for (int i = 1; i < argc; i++) {
+        char* arg = argv[i];
+        if (streq(arg, "-n") || streq(arg, "--new")) {
+            forceNew = true;
+            continue;
         }
+        char* field = strchr(arg, '=');
+        if (field != nullptr) {
+            *field = '\0';
+            field++;
+            if (streq(arg, "--CC")) {
+                default_cc = field;
+                continue;
+            }
+            if (streq(arg, "--GCC")) {
+                default_gcc = field;
+                continue;
+            }
+            if (streq(arg, "--CFLAGS")) {
+                default_cflags = field;
+                continue;
+            }
+            if (streq(arg, "--BFLAGS")) {
+                default_bflags = field;
+                continue;
+            }
+            if (streq(arg, "--DFLAGS")) {
+                default_dflags = field;
+                continue;
+            }
+            if (streq(arg, "--LFLAGS")) {
+                default_lflags = field;
+                continue;
+            }
+            if (streq(arg, "--LIBS")) {
+                default_libs = field;
+                continue;
+            }
+        }
+        
+        // invalid argument
+        cout << "Unparseable argument: " << arg << endl;
+        cout << "Usage: " << argv[0] << " [-n] [--<field name>=<default value>]" << endl;
+        return EXIT_FAILURE;
+    }
 
+    try {
+        // read
+        Makefile m;
+        if (!forceNew) m = read_makefile();
         // crawl
         crawl_files(m);
         // write
         write_makefile(m);
     } catch (runtime_error* re) {
-        cout << re->what() << endl;
+        cout << re->what();
         delete re;
         return EXIT_FAILURE;
     }
